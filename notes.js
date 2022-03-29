475a2aab6508881577a139b3297fa256bc4d4062
Jonathon Squire <jgsquire27@gmail.com>
2022-03-29 08:58:10 -0600

    initial

---
+++ notes.js
@@ -0,0 +1,43 @@
+//binary search tree rough structure
+
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(element) {}
+  find(key) {}
+  remove(key) {}
+  size() {}
+  print() {}
+}
+
+class Node{
+  constructor(element) {
+    this.element = element;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+//Binary Heap
+
+
+class BinaryHeap {
+  constructor() {
+    this.root = null;
+  }
+  insert(element) {}
+  find(element) {}
+  remove(element) {}
+  extractMax(element) {}
+}
+
+
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.right = null;
+    this.left = null;
+  }
+}
